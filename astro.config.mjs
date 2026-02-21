c77b6f6fb916ecdb77933598d4208eedd49a565e
Shark <sharktheone@proton.me>
2026-02-21 22:08:16 +0100

    opus 4.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,3 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
-});
+export default defineConfig({});
